7f04445dd8556f770cb76c374cec6328efcb9dca
SHREYK213 <shreyaspoojary240@gmail.com>
2023-06-03 23:21:20 +0530

    Resume page update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -36,6 +36,11 @@ function ResumeNew() {
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button

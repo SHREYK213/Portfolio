159f94d3816aa532f9c78cf30b008f3e314e854d
shreyas <shreyaspoojary240@gmail.com>
2023-09-28 10:42:10 +0530

    Resume removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -87,7 +87,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/resume"
+                to=""
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
